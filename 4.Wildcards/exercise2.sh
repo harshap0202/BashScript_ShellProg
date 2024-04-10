70cf27f1f29007c1469321a26c5117b0f95158e0
Harsh <116268106+harshap0202@users.noreply.github.com>
2024-04-10 12:16:40 +0530

    Update exercise2.sh

--- 4.Wildcards/exercise2.sh
+++ 4.Wildcards/exercise2.sh
@@ -4,7 +4,7 @@ read -p "Please enter a file extension: " EXT
 
 DATE=$(date '+%Y-%m-%d')
 
-read -p "Please enter a file prefix (Press ENTER for 2015 08 10) : " PRE
+read -p "Please enter a file prefix (Press ENTER for Date) : " PRE
 if [ -z "$PRE" ]
 then
 	for LIST in $(ls *.${EXT})
